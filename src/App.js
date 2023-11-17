2ebdbfc6a47ef139d3cf347cc323512e4b13703c
GolamAhammed <devgolamgit@gmail.com>
2023-11-17 16:54:19 +0530

    added details page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./Components/Header";
 import Cards from "./Components/Cards";
+import Detail from "./Components/Detail";
 import AddMovie from "./Components/AddMovie";
 import { Route,Routes } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
               <Route path="/" element={<Cards/>} />
               <Route path="/Addmovie" element={<AddMovie/>} />
+              <Route path="/detail/:id" element={<Detail/>} />
       </Routes>
     </div>
   );
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { TailSpin,ThreeDots } from "react-loader-spinner";
 import ReactStars from 'react-stars'
 import { moviesRef } from "./Firebase/Firebase";
+import { Link } from "react-router-dom";
 
 const Cards = () => {
     const[data,setData]=useState([ ]);
@@ -15,7 +16,7 @@ const Cards = () => {
         const _data = await getDocs(moviesRef);
         
         _data.forEach ((doc) =>{
-          setData((prv)=> [...prv ,doc.data()])
+          setData((prv)=> [...prv ,{...(doc.data()), id: doc.id}])
         })
         setLoading(false);
       }
@@ -28,7 +29,7 @@ const Cards = () => {
        data.map((e,i)=>{
         return (
       
-            <div key={i} className="bg-gray-900 shadow-lg
+            <Link to={`/detail/${e.id}`}><div key={i} className="bg-gray-900 shadow-lg
             text-sm font-medium p-2 hover:-translate-y-3 cursor-pointer  mt-6 transition-all duration-500 ">
             <img className="h-60 md:h-72" src={e.image}/>
             <h1> 
@@ -46,7 +47,7 @@ const Cards = () => {
               <span className="text-gray-500">Year:</span>   {e.year}
             
             </h1>
-            </div>
+            </div></Link>
          );
         })}
       
